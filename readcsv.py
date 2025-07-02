a994f3f2ab015de15971db1d66ac52d968d43b7b
pskulkarni7 <pallaviskulkarni7@gmail.com>
2025-07-02 12:17:05 +0800

    updated weather

---
+++ readcsv.py
@@ -0,0 +1,11 @@
+import csv
+# Read the CSV file and print its contents
+
+with open('weather.csv', 'r') as file:
+    reader = csv.reader(file)
+    data = list(reader)
+    for line in data:   
+        if line:
+            # Assuming the CSV format is: City, Condition, Temperature
+            print(f"It is currently {line[2]}\u00b0C and {line[1]} in {line[0]} city.")
+        
