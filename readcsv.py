3c341555475960c1ae4e1ced41ffaa3bd9716861
pskulkarni7 <pallaviskulkarni7@gmail.com>
2025-07-05 18:10:43 +0800

    Added exception handling

--- readcsv.py
+++ readcsv.py
@@ -5,7 +5,9 @@ with open('temperatures.csv', 'r') as file:
     reader = csv.reader(file)
     data = list(reader)
     for line in data:   
-        if line:
             # Assuming the CSV format is: City, Condition, Temperature
-            print(f"It is currently {line[2]}\u00b0C and {line[1]} in {line[0]} city.")
+            try:
+                print(f"It is currently {line[2]}\u00b0C and {line[1]} in {line[0]} city.")
+            except IndexError:  
+                print("No City data found.")
         
